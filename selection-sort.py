51004d84cc729379af156271785740d25c6cfd17
Marco Baggio <marcobgg4@gmail.com>
2018-02-28 17:54:02 +0100

    selection sort python

---
+++ selection-sort.py
@@ -0,0 +1,15 @@
+A=[5,4,3,2,1]
+n=len(A)
+j=0
+while j<n-1:
+    smallest=j
+    i=j+1
+    while i<n:
+        if A[i]<A[smallest]:
+            smallest=i
+        i=i+1
+    temp=A[j]
+    A[j]=A[smallest]
+    A[smallest]=temp
+    j=j+1
+print(A)
